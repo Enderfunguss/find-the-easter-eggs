629ebba8f604be29b23402ec84e057cb65a3c763
Ender!11!!1!! <76421494+Enderfunguss@users.noreply.github.com>
2021-06-08 21:12:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function preload(){
+
+}
+
+function setup(){
+
+}
+
+function draw(){
+
+}
+
+function personButtonInfo(){
+    window.alert('Arent things just easier when you ask? This button tells which person the webcam can see!');
+}
+
+function accuracyButtonInfo(){
+    window.alert('Beep skaboop beep boop. The accuracy is shown here!');
+}
+
+function secretClick(){
+    var audio = new Audio('wonderful-idea.ogg');
+    audio.play();
+    window.alert('Go to f11 to see the full website background.');
+}
